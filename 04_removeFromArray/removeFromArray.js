bfede94b234f8a0e48c966add1b9c1d2444abdc1
nathanm4 <nathan.mukku@gmail.com>
2022-08-18 13:52:26 -0400

    remove one element passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(input) {
+    const args = Array.from(arguments);
+    const original = args[0];
+    let result = original;
 
+    for(let i = 1; i <= args.length; ++i){
+        let index = original.indexOf(args[i]);
+        if(index > -1){
+            result.splice(index, 1);
+        }
+    }
+    return result;
 };
 
 // Do not edit below this line
