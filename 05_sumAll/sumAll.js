b2b7c5249dbac57a09dd32135c00af1b4c8dba59
nathanm4 <nathan.mukku@gmail.com>
2022-08-18 14:37:49 -0400

    passed all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -3,6 +3,10 @@ const sumAll = function(num1 ,  num2) {
     let y = num2;
     let sum = 0;
 
+
+    if (!Number.isInteger(x) || !Number.isInteger(y)) return 'ERROR';
+    if (x < 0 || y < 0) return 'ERROR';
+
     if (x < y) {
         for (let i = x; i <=y; i++) {
             sum += i;
