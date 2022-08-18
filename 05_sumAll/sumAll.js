519a009a9002598293a65fbc53533e7f064869ad
nathanm4 <nathan.mukku@gmail.com>
2022-08-18 14:15:22 -0400

    pass 3rd test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -3,11 +3,18 @@ const sumAll = function(num1 ,  num2) {
     let y = num2;
     let sum = 0;
 
-    for (let i = x; i <=y; i++) {
-        sum += i;
+    if (x < y) {
+        for (let i = x; i <=y; i++) {
+            sum += i;
+        }
+        return sum;
+    } else if (x > y) {
+        for (let i = y; i <= x; i++) {
+        sum+= i;
+        }
+        return sum;
     }
-    return sum;
-};
+}
 
 // Do not edit below this line
 module.exports = sumAll;
