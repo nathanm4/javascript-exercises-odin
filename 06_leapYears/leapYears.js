9158bd4806c355f41726168a6db673deb5cda6ab
nathanm4 <nathan.mukku@gmail.com>
2022-08-18 14:54:50 -0400

    passd all the tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,18 +1,6 @@
-const leapYears = function(args) {
+const leapYears = function(year) {
 
-    const year = args;
-if (year % 100 === 0 && year % 400 === 0) {
-    return true;
-}
-
-
-
-if (year % 4 === 0) {
-    return true;
-}
-
-return false;
-
+    return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
 };
 
 // Do not edit below this line
