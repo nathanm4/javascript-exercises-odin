bb679e1847e1368401611a0568d1993f6793878b
nathanm4 <nathan.mukku@gmail.com>
2022-08-18 13:19:15 -0400

    reversed single word

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let splitString = string.split(''); //split string into array with indv letters
+    let reverseArray = splitString.reverse(); //reverse the elements of the array
+    let joinedArray = reverseArray.join(''); //join all elements of the array into reg string
+    return joinedArray;
 };
 
 // Do not edit below this line
