8a9a156fa4783c25a93f01b0248646f80232ac1b
nathanm4 <nathan.mukku@gmail.com>
2022-08-18 14:11:05 -0400

    pass 1st test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
+const sumAll = function(num1 ,  num2) {
+    let x = num1;
+    let y = num2;
+    let sum = 0;
 
+    for (let i = x; i <=y; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
