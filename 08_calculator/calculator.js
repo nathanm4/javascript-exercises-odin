785f0e41d08929967c8bfaad7195c4b38a897230
nathanm4 <nathan.mukku@gmail.com>
2022-08-25 16:02:43 -0400

    idk

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,17 +1,28 @@
-const add = function() {
-	
+// add, subtract, get the sum, multiply, get the power, and find the factorial
+
+
+const add = function(a , b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+    let sum = 0;
+    for (let i in a) {
+        sum+= a[i];
+    }
+    return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+    let product = 1;
+    for (let i in a) {
+        product*= a[i];
+    }
+    return product;
 };
 
 const power = function() {
