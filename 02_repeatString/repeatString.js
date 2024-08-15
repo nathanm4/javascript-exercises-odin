9a2033f85acc43eb3df65e76fc4f196cc5903f86
nathanm4 <nathan.mukku@gmail.com>
2024-08-15 11:36:43 -0400

    finished excercise 2 with using .repeat() method

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, count) {
+    if (count < 0) {
+        return "ERROR";
+    }
+    for (let index = 0; index <= count; index++) {
+        return string.repeat(count);
+    }
 };
 
 // Do not edit below this line
